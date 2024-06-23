70425bd88345eed738aed251d9c732526968cbde
Ashish Kumar <ashishkumar901336@gmail.com>
2024-06-23 15:38:59 +0530

    feat: nickname, online, join/left

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
-const { Server } = require("socket.io");
+const { Server } = require('socket.io');
 const io = new Server(server);
+
+let users = {};
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html");
@@ -11,14 +13,34 @@ app.get("/", (req, res) => {
 
 io.on("connection", (socket) => {
     console.log("a user connected!");
+
+    socket.on("set nickname", (nickname) => {
+        users[socket.id] = nickname;
+        io.emit("user list", Object.values(users));
+        socket.broadcast.emit("user connected", `${nickname} has joined the chat`);
+    });
+
     socket.on("disconnect", () => {
+        if (users[socket.id]) {
+            socket.broadcast.emit("user disconnected", `${users[socket.id]} has left the chat`);
+            delete users[socket.id];
+            io.emit("user list", Object.values(users));
+        }
         console.log("user disconnected!");
     });
+
     socket.on("chat message", (msg) => {
-        io.emit("chat message", msg);
+        socket.broadcast.emit("chat message", { user: users[socket.id], msg });
+    });
+
+    socket.on("typing", () => {
+        socket.broadcast.emit("typing", users[socket.id]);
+    });
+
+    socket.on("stop typing", () => {
+        socket.broadcast.emit("stop typing", users[socket.id]);
     });
 });
-
 
 server.listen(3000, () => {
     console.log('listening on *:3000');
